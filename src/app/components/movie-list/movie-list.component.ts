21ab29950ad3dd691889cc004da803ef55cb9f6f
Tim Sundell <tim920129@hotmail.com>
2023-09-05 10:56:56 +0200

    :sparkles: Gave movies correct category values

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -9,11 +9,42 @@ import { MovieService } from 'src/app/services/movie.service';
 export class MovieListComponent {
   movieList: IMovie[] = [];
   movieService: MovieService = inject(MovieService);
+  getData: IMovie[] | null = localStorage.getItem('movies') ? JSON.parse(localStorage.getItem('movies')!) : null;
 
   ngOnInit() {
-    this.movieService.getMovies().subscribe(movies => {
-      this.movieList = movies;
+    if (this.getData === null) {
+      this.movieService.getMovies().subscribe(movies => {
+        this.movieList = movies;
+        this.setCategoryName();
+        localStorage.setItem('movies', JSON.stringify(this.movieList));
+      });
+    } else {
+      this.movieList = this.getData;
       console.log(this.movieList);
+    }
+  }
+  setCategoryName() {
+    this.movieList.forEach(movie => {
+      movie.productCategory.forEach(movie => {
+        if (movie.category === null) {
+          switch (movie.categoryId) {
+            case 5:
+              movie.category = 'Action';
+              break;
+            case 6:
+              movie.category = 'Thriller';
+              break;
+            case 7:
+              movie.category = 'Comedy';
+              break;
+            case 8:
+              movie.category = 'Sci-Fi';
+              break;
+            default:
+              break;
+          }
+        }
+      });
     });
   }
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -2,16 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IMovie } from '../models/IMovie';
+import { ICategory } from '../models/ICategory';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
+  baseUrl: string = 'https://medieinstitutet-wie-products.azurewebsites.net/api/';
+
   protected movieList: IMovie[] = [];
   constructor(private http: HttpClient) {}
 
   getMovies(): Observable<IMovie[]> {
-    const apiUrl = 'https://medieinstitutet-wie-products.azurewebsites.net/api/products';
+    const apiUrl = this.baseUrl + 'products';
     return this.http.get<IMovie[]>(apiUrl);
+  }
+
+  getCategories(): Observable<ICategory[]> {
+    const apiUrl = this.baseUrl + 'categories';
+    return this.http.get<ICategory[]>(apiUrl);
   }
 }
