b6b7dd8d5b2c6ac864d6b39e8ea71ef9fa19bf04
Mattias heurlin <mattias.heurlin@hotmail.com>
2023-09-06 13:32:03 +0200

    Cart functionality added ✨

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { IMovie } from 'src/app/models/IMovie';
 import { ImageService } from 'src/app/services/image.service';
 
@@ -8,10 +8,15 @@ import { ImageService } from 'src/app/services/image.service';
 })
 export class MovieListComponent {
   @Input() movieList: IMovie[] = [];
+  @Output() newItem: EventEmitter<IMovie> = new EventEmitter<IMovie>();
 
   constructor(private imageService: ImageService) {}
 
   handleImg(event: Event) {
     this.imageService.handleBrokenImgLink(event);
+  }
+
+  movieAdded(movie: IMovie) {
+    this.newItem.emit(movie);
   }
 }
